008b7c46ee8fc78f9fc9a559882b7df50a8a68f8
Blaise Wielk <bwielk@gmail.com>
2021-12-12 18:52:38 +0000

    implemented the logic for the fish' reproduction countdown

---
+++ lanternfish/src/main/java/Lanternfish.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lanternfish {
+
+	private static List<Integer> initialState;
+	private static List<ArrayList<Integer>> dayRecords = new ArrayList<>();
+	private static List<Integer> newFish = new ArrayList<>();
+
+	public static void main( String[] args ) {
+		readInitialState();
+		for(int i=0; i<5; i++){
+			List<Integer> currentDayRecord = dayRecords.get( i );
+			ArrayList<Integer> newDayList = new ArrayList<>();
+			for(int x=0; x<currentDayRecord.size(); x++){
+				int currentIntervalNumber =  currentDayRecord.get( x );
+				int newIntervalNumber;
+				if(currentIntervalNumber-1<0){
+					newIntervalNumber = 6;
+					newDayList.add( newIntervalNumber );
+					newFish.add( 8 );
+				}else{
+					newDayList.add( currentIntervalNumber-1);
+				}
+			}
+			newDayList.addAll( newFish );
+			dayRecords.add( newDayList );
+			newFish = new ArrayList<>();
+		}
+		System.out.println(dayRecords);
+	}
+
+	private static void readInitialState(){
+		ClassLoader classLoader = Lanternfish.class.getClassLoader();
+		InputStream is = classLoader.getResourceAsStream( "f.txt" );
+		try( InputStreamReader streamReader = new InputStreamReader( is, StandardCharsets.UTF_8 );
+				BufferedReader reader = new BufferedReader( streamReader )) {
+			String line;
+			while((line = reader.readLine()) != null) {
+				initialState = Arrays.asList(line.split( "," )).stream().map( Integer::parseInt ).collect(
+						Collectors.toList());
+			}
+		} catch ( IOException e ) {
+			e.printStackTrace();
+		}
+		dayRecords.add( (ArrayList<Integer>) initialState );
+	}
+}
